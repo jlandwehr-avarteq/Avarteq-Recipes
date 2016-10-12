7a461ca43066935e00dd981c092afdfc493fab5a
Janis Landwehr <jlandwehr@avarteq.de>
2016-10-12 10:10:32 +0200

    Adjust format
    Login was not centered
    
    * Center login forms
    * Fix minor mistakes
    * Create active functionality in header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-  def active_class(link_path)
+  #should be used like: class="<%= is_active?(new_user_session_path)%>" inside li for header items
+  def is_active?(link_path)
     current_page?(link_path) ? "active" : ""
   end
 end
