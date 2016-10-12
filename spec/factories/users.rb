225ef333f24cb90a12cb5fe6d00be1a058985f4d
Janis Landwehr <jlandwehr@avarteq.de>
2016-10-12 16:12:18 +0200

    Add tests files
    * Add tests
    * Fix error with factory girl

---
+++ spec/factories/users.rb
@@ -0,0 +1,22 @@
+FactoryGirl.define do
+  factory :user do
+    username              FFaker::Internet.user_name
+    first_name            FFaker::Name.first_name
+    last_name             FFaker::Name.last_name
+    email                 FFaker::Internet.email
+    password              "password"
+    password_confirmation "password"
+  end
+
+    factory :user_wrong_pw, parent: :user do |u|
+      u.password_confirmation "wrong_pw"
+    end
+
+    factory :user_invalid_username, parent: :user do |u|
+      u.username nil
+    end
+
+    factory :user_invalid_email, parent: :user do |u|
+      u.email nil
+    end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'valid arguments' do
+    subject { build(:user) }
+
+    it { is_expected.to be_valid}
+    it { expect(subject.username).not_to be_nil}
+    it { expect(subject.first_name).not_to be_nil}
+    it { expect(subject.last_name).not_to be_nil}
+    it { expect(subject.email).not_to be_nil}
+    it { expect(subject.password).not_to be_nil}
+    it { expect(subject.password_confirmation).not_to be_nil}
+
+    it 'should save a valid user' do
+      subject.save
+      expect(User.all).to include(subject)
+    end
+  end
+
+  describe 'invalid email' do
+    subject { build(:user_invalid_email)}
+
+    it { is_expected.to be_invalid}
+
+    it 'should not save an invalid user' do
+      subject.save
+
+      expect(User.all).not_to include(subject)
+    end
+  end
+
+  describe 'invalid username' do
+    subject { build(:user_invalid_username)}
+
+    it { is_expected.to be_invalid}
+
+    it 'should not save an invalid user' do
+      subject.save
+
+      expect(User.all).not_to include(subject)
+    end
+  end
+
+  describe 'invalid actions' do
+    user1 = FactoryGirl.build(:user)
+    user2 = FactoryGirl.build(:user)
+
+    it 'should not save the same email twice' do
+      user1.save
+      user2.email = user1.email
+      user2.save
+
+      expect(user2.errors).not_to be_empty
+    end
+
+    it 'should not save the same username twice' do
+      user1.save
+      user2.email = user1.username
+      user2.save
+
+      expect(user2.errors).not_to be_empty
+    end
+  end
 end
