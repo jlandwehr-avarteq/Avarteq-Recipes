b41d3076207afea6b1ccfa9037a6010897c7effd
Janis Landwehr <jlandwehr@avarteq.de>
2016-10-13 16:00:36 +0200

    Apply changes according to the code review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  #should be used like: class="<%= is_active?(new_user_session_path)%>" inside li for header items
+  #should be used like: class="<%= is_active?(new_user_session_path) %>" inside li for header items
   def is_active?(link_path)
-    current_page?(link_path) ? "active" : ""
+    current_page?(link_path) ? 'active' : ''
   end
 end
